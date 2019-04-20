d7091dcce980378f67e567bfddf15f181b2bee74
TheZihanGu <39030261+TheZihanGu@users.noreply.github.com>
2019-04-20 20:37:44 +0800

    对页面内容进行翻译

--- zip2res/upload_file.php
+++ zip2res/upload_file.php
@@ -1,20 +1,20 @@
 ﻿<!DOCTYPE html>
-<html lang="zh-CN">
+<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta name="renderer" content="webkit">
 		<meta name="theme-color" content="#3F51B5" />
-		<title>米环交流会 - RES反编译工具</title>
-		<link href="https://cdn.bootcss.com/mdui/0.4.2/css/mdui.min.css" rel="stylesheet">
+		<title>MIBBS - RES Decompiler Tool</title>
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.2/css/mdui.min.css">
 	</head>
 
 	<body class="mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink">
 		<header class="mdui-appbar mdui-appbar-fixed">
 			<div class="mdui-toolbar mdui-color-theme">
 				<span class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white" mdui-drawer="{target: '#main-drawer', swipe: true}"><i class="mdui-icon material-icons">menu</i></span>
-				<a href="https://rmpai.com" target="_blank" class="mdui-typo-headline mdui-hidden-xs">米环交流会 - RES反编译工具</a>
+				<a href="https://en.rmpai.com" target="_blank" class="mdui-typo-headline mdui-hidden-xs">MIBBS - RES Decompiler Tool</a>
 			</div>
 		</header>
 
@@ -25,7 +25,7 @@
 				<li class="mdui-list-item mdui-ripple">
 					<i class="mdui-list-item-icon mdui-icon material-icons mdui-text-color-light-blue">home</i>
 					<div class="mdui-list-item-content">
-						<a href="https://rmpai.com" class="mdui-ripple">回到首页</a>
+						<a href="https://en.rmpai.com" class="mdui-ripple">Back to Home</a>
 					</div>
 					</div>			
 				</li>
@@ -39,11 +39,10 @@
                         <?php
 						error_reporting(0);
 						header("Refresh:10;url=cache/" . getSubstr($_FILES["file"]["name"],'','.zip') . "_packed.res");
-						// 自适应布局 echo "<meta name="\"viewport"\" content="\"width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no"\">"
 						$allowedExts = array("zip");
 						$temp = explode(".", $_FILES["file"]["name"]);
 						echo $_FILES["file"]["size"];
-						$extension = end($temp);     // 获取文件后缀名
+						$extension = end($temp);
 						if ((($_FILES["file"]["type"] == "application/zip")
 						|| ($_FILES["file"]["type"] == "application/zip")
 						|| ($_FILES["file"]["type"] == "application/x-zip-compressed")
@@ -51,37 +50,34 @@
 						|| ($_FILES["file"]["type"] == "image/pjpeg")
 						|| ($_FILES["file"]["type"] == "image/x-png")
 						|| ($_FILES["file"]["type"] == "image/png"))
-						&& ($_FILES["file"]["size"] < 2*1024*1024)   // 小于 2MB
+						&& ($_FILES["file"]["size"] < 2*1024*1024)
 						&& in_array($extension, $allowedExts))
 						{
     						if ($_FILES["file"]["error"] > 0)
     						{
-        						echo "错误：: " . $_FILES["file"]["error"] . "<br>";
+        						echo "error: " . $_FILES["file"]["error"] . "<br>";
     						}
     						else
     						{
-        						echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
-        						echo "服务器正在处理你的文件，稍后会自动下载zip压缩包！" . "<br>";
+        						echo "Upload file name: " . $_FILES["file"]["name"] . "<br>";
+        						echo "The server is working on your files and will automatically download res packages later!" . "<br>";
         
-        						// 判断当期目录下的 upload 目录是否存在该文件
-        						// 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
         						if (file_exists("upload/" . $_FILES["file"]["name"]))
         						{
-            						echo $_FILES["file"]["name"] . " 文件已经存在。 ";
+            						echo $_FILES["file"]["name"] . " file already exist. ";
         						}
         						else
         						{
-            						// 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
             						move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
-            						echo "文件存储在: " . "upload/" . $_FILES["file"]["name"];
+            						echo "Files are stored in: " . "upload/" . $_FILES["file"]["name"];
 									echo "<br>";
-									echo "请耐心等待约10秒<br>";
+									echo "Please wait patiently for about 10 seconds.<br>";
         						}
     						}
 						}
 						else
 						{
-    						echo "非法的文件格式";
+    						echo "Illegal file format.";
 							echo $_FILES["file"]["type"];
 						}
 						getSubstr($_FILES["file"]["name"],'','.res');
@@ -89,9 +85,7 @@
 						function getSubstr($str, $leftStr, $rightStr)
 						{
     						$left = strpos($str, $leftStr);
-    						//echo '左边:'.$left;
     						$right = strpos($str, $rightStr,$left);
-    						//echo '<br>右边:'.$right;
     						if($left < 0 or $right < $left) return '';
     						return substr($str, $left + strlen($leftStr), $right-$left-strlen($leftStr));
 						}
@@ -105,6 +99,6 @@
             </div>
         </div>
 	</body>
-	<script src="https://cdn.bootcss.com/mdui/0.4.2/js/mdui.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.2/js/mdui.min.js"></script>
 	<script src="./js/like.js"></script>
 </html>
