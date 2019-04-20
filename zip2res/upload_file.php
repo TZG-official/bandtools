ecd210b76c11612cd1c60bb5665733683015a4a5
TheZihanGu <39030261+TheZihanGu@users.noreply.github.com>
2019-04-20 19:53:52 +0800

    对upload_file.php提示内容进行翻译

--- zip2res/upload_file.php
+++ zip2res/upload_file.php
@@ -1,11 +1,10 @@
 <?php
 error_reporting(0);
 header("Refresh:10;url=cache/" . getSubstr($_FILES["file"]["name"],'','.zip') . "_packed.res");
-// 自适应布局 echo "<meta name="\"viewport"\" content="\"width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no"\">"
 $allowedExts = array("zip");
 $temp = explode(".", $_FILES["file"]["name"]);
 echo $_FILES["file"]["size"];
-$extension = end($temp);     // 获取文件后缀名
+$extension = end($temp);
 if ((($_FILES["file"]["type"] == "application/zip")
 || ($_FILES["file"]["type"] == "application/zip")
 || ($_FILES["file"]["type"] == "application/x-zip-compressed")
@@ -13,56 +12,45 @@ if ((($_FILES["file"]["type"] == "application/zip")
 || ($_FILES["file"]["type"] == "image/pjpeg")
 || ($_FILES["file"]["type"] == "image/x-png")
 || ($_FILES["file"]["type"] == "image/png"))
-&& ($_FILES["file"]["size"] < 2*1024*1024)   // 小于 2MB
+&& ($_FILES["file"]["size"] < 2*1024*1024)
 && in_array($extension, $allowedExts))
 {
     if ($_FILES["file"]["error"] > 0)
     {
-        echo "错误：: " . $_FILES["file"]["error"] . "<br>";
+        echo "error: " . $_FILES["file"]["error"] . "<br>";
     }
     else
     {
-        echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
-        echo "服务器正在处理你的文件，稍后会自动下载zip压缩包！" . "<br>";
+        echo "Upload file name: " . $_FILES["file"]["name"] . "<br>";
+        echo "The server is processing your files, and will download res package automatically later" .  "<br>";
         
 		
         
-        
-        // 判断当期目录下的 upload 目录是否存在该文件
-        // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
         if (file_exists("upload/" . $_FILES["file"]["name"]))
         {
-            echo $_FILES["file"]["name"] . " 文件已经存在。 ";
+            echo $_FILES["file"]["name"] . " file already exist. ";
         }
         else
         {
-            // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
             move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
-            echo "文件存储在: " . "upload/" . $_FILES["file"]["name"];
+            echo "Files are stored in: " . "upload/" . $_FILES["file"]["name"];
 			echo "<br>";
-			echo "请耐心等待约10秒<br>";
+			echo "Please wait patiently for about 10 seconds<br>";
         }
     }
 }
 else
 {
-    echo "非法的文件格式";
+    echo "Illegal file format";
 	echo $_FILES["file"]["type"];
 }
 getSubstr($_FILES["file"]["name"],'','.res');
  
-/*以下是取中间文本的函数 
-  getSubstr=调用名称
-  $str=预取全文本 
-  $leftStr=左边文本
-  $rightStr=右边文本
-*/
+
 function getSubstr($str, $leftStr, $rightStr)
 {
     $left = strpos($str, $leftStr);
-    //echo '左边:'.$left;
     $right = strpos($str, $rightStr,$left);
-    //echo '<br>右边:'.$right;
     if($left < 0 or $right < $left) return '';
     return substr($str, $left + strlen($leftStr), $right-$left-strlen($leftStr));
 }
