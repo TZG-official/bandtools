706b02723b49ff283e5ca85f5f263442f8c7e19a
TheZihanGu <i@zihangu.cn>
2019-08-01 20:00:32 +0800

    全站加入Matomo跟踪

--- res2zip/upload_file.php
+++ res2zip/upload_file.php
@@ -1,6 +1,23 @@
 ﻿<!DOCTYPE html>
 <html lang="zh-CN">
-	<head>
+
+	<!-- Matomo -->
+	<script type="text/javascript">
+	var _paq = window._paq || [];
+	/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+	_paq.push(['trackPageView']);
+	_paq.push(['enableLinkTracking']);
+	(function() {
+	  var u="//s.tzg6.com/";
+	  _paq.push(['setTrackerUrl', u+'matomo.php']);
+	  _paq.push(['setSiteId', '4']);
+	  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+	})();
+  	</script>
+  	<!-- End Matomo Code -->
+    
+      <head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
--- zip2res/upload_file.php
+++ zip2res/upload_file.php
@@ -1,5 +1,22 @@
 ﻿<!DOCTYPE html>
 <html lang="zh-CN">
+
+	<!-- Matomo -->
+	<script type="text/javascript">
+	var _paq = window._paq || [];
+	/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+	_paq.push(['trackPageView']);
+	_paq.push(['enableLinkTracking']);
+	(function() {
+	  var u="//s.tzg6.com/";
+	  _paq.push(['setTrackerUrl', u+'matomo.php']);
+	  _paq.push(['setSiteId', '4']);
+	  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+	})();
+  	</script>
+	<!-- End Matomo Code -->
+	  
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no" />
